053b4ab289367c3f5cb46eebaad8e5ff3e82e82a
CMilanoUCA <cmilano@cub.uca.edu>
2025-01-21 17:26:28 -0600

    Trying to initialize and fix up all classes/parameters with little success

--- src/Purse.java
+++ src/Purse.java
@@ -1,12 +1,12 @@
 import java.util.Map;
-
+import java.util.Integer;
 
 
 public class Purse {
     public static void main(String[] args) {
-    Map cash<Denomination, Integer>             // represents the money in the purse
-    void add(Denomination, int num)                 // adds a number of a particular denomination
-    double remove(Denomination type, int num) //diminishes the money in the purse and returns that amount.
+    Map cash<Denomination.amt, Integer>             // represents the money in the purse
+    void add(Denomination.amt, int num)                 // adds a number of a particular denomination
+    double remove(Denomination.form, num) //diminishes the money in the purse and returns that amount.
     double getValue()                           // returns the amount of money in the Purse
     String toString()                           // returns a string representation of the Purse and its contents
     }
